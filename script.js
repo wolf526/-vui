4d9f25dd6e771784aa5658f475e57cd19af5d022
wolf526 <builananh24021986@gmail.com>
2025-03-14 22:32:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
 const questions = [
   {
     question: "Thủ đô của Việt Nam là gì?",
